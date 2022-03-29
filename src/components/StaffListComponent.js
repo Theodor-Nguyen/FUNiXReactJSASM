21d2d8bc8301f32a89bea075fc5d78f5f412ac57
Theodor Nguyen <theodornguyen22@gmail.com>
2022-03-29 22:35:08 +0700

    Fix renderStaff function

--- src/components/StaffListComponent.js
+++ src/components/StaffListComponent.js
@@ -1,11 +1,5 @@
 import React, { Component } from "react";
-import {
-  Card,
-  CardImg,
-  CardText,
-  CardBody,
-  CardTitle,
-} from "reactstrap";
+import { Card, CardImg, CardText, CardBody, CardTitle } from "reactstrap";
 import dateFormat from "dateformat";
 
 class StaffList extends Component {
@@ -21,29 +15,33 @@ class StaffList extends Component {
     this.setState({ selectedStaff: staff });
   }
   renderStaff(staff) {
-    if (staff != null) {
+    console.log(staff);
+    if (staff) {
       return (
-    
-        <Card>
-          <CardImg width="100%" src={staff.image} alt={staff.name} />
-          <CardBody>
-            <CardTitle>Họ và tên: {staff.name}</CardTitle>
-            <CardText>
-              Ngày sinh: {dateFormat(staff.doB, "dd/mm/yyyy")}
-            </CardText>
-            <CardText>
-              Ngày vào công ty: {dateFormat(staff.startDate, "dd/mm/yyyy")}
-            </CardText>
-            <CardText>Phòng ban: {staff.department.name}</CardText>
-            <CardText>Số ngày nghỉ còn lại: {staff.annualLeave}</CardText>
-            <CardText>Số ngày đã làm thêm: {staff.overTime}</CardText>
-          </CardBody>
-        </Card>
+        <div className={this.state.gridDefault}>
+          <Card>
+            <CardImg width="100%" src={staff.image} alt={staff.name} />
+            <CardBody>
+              <CardTitle>Họ và tên: {staff.name}</CardTitle>
+              <CardText>
+                Ngày sinh: {dateFormat(staff.doB, "dd/mm/yyyy")}
+              </CardText>
+              <CardText>
+                Ngày vào công ty: {dateFormat(staff.startDate, "dd/mm/yyyy")}
+              </CardText>
+              <CardText>Phòng ban: {staff.department.name}</CardText>
+              <CardText>Số ngày nghỉ còn lại: {staff.annualLeave}</CardText>
+              <CardText>Số ngày đã làm thêm: {staff.overTime}</CardText>
+            </CardBody>
+          </Card>
+        </div>
       );
     } else {
-      <div>
-        <p>Bấm vào tên nhân viên để xem thông tin.</p>
-      </div>;
+      return (
+        <div className={this.state.gridDefault}>
+          <p>Bấm vào tên nhân viên để xem thông tin.</p>
+        </div>
+      );
     }
   }
 
@@ -62,7 +60,7 @@ class StaffList extends Component {
     return (
       <div className="container">
         <div className="row">{staffsList}</div>
-        <div className="row">{this.onStaffSelect(this.state.selectedStaff)}</div>
+        <div className="row">{this.renderStaff(this.state.selectedStaff)}</div>
       </div>
     );
   }
