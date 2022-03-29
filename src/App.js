e29f81a9a533c78a9aae1e2f3a3510ac0528e8a9
Theodor Nguyen <theodornguyen22@gmail.com>
2022-03-29 12:05:47 +0700

    Configuring your React Application

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
+import React, { Component } from 'react';
+import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
 
-function App() {
-
+class App extends Component {
+render() {
   return (
     <div>
-     
+     <Navbar dark color="primary">
+       <div className='container'>
+         <NavbarBrand href="/">Ứng dụng quản lý nhân sự v1.0</NavbarBrand>
+       </div>
+     </Navbar>
     </div>
   );
+ }
 }
 
 export default App;
