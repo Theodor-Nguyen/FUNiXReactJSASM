cd673c7715967fed16d716b9c35030e1e104fbcf
Theodor Nguyen <theodornguyen22@gmail.com>
2022-03-29 18:45:03 +0700

    Create UI for StaffList

--- src/components/StaffListComponent.js
+++ src/components/StaffListComponent.js
@@ -2,12 +2,31 @@ import React, { Component } from 'react';
 import {Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle} from "reactstrap";
 
 class StaffList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            gridDefault: "col-12 col-md-6 col-lg-4 mt-2"
+        };
+    }
     
     render() {
+        const staffsList = this.props.staffs.map((staff) => {
+            return (
+                <div key={staff.id} className={this.state.gridDefault}>
+                    <Card>
+                        <CardBody>
+                            <CardTitle>{staff.name}</CardTitle>
+                        </CardBody>
+                    </Card>
+                </div>
+            );
+        });
         return (
             <div className='container'>
                 <div className='row'>
+                {staffsList}
                 </div>
+                <p>Bấm vào tên nhân viên để xem thông tin.</p>
             </div>
         );
     }
