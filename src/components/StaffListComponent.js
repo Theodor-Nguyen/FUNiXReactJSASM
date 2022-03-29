c9ea3ab3a91f1ca414b5046474ce2cb6fad19320
Theodor Nguyen <theodornguyen22@gmail.com>
2022-03-29 21:36:52 +0700

    Build StaffListComponent

--- src/components/StaffListComponent.js
+++ src/components/StaffListComponent.js
@@ -1,35 +1,71 @@
-import React, { Component } from 'react';
-import {Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle} from "reactstrap";
+import React, { Component } from "react";
+import {
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+} from "reactstrap";
+import dateFormat from "dateformat";
 
 class StaffList extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            gridDefault: "col-12 col-md-6 col-lg-4 mt-2"
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      gridDefault: "col-12 col-md-6 col-lg-4 mt-2",
+      selectedStaff: null,
+    };
+  }
+
+  onStaffSelect(staff) {
+    this.setState({ selectedStaff: staff });
+  }
+  renderStaff(staff) {
+    if (staff != null) {
+      return (
     
-    render() {
-        const staffsList = this.props.staffs.map((staff) => {
-            return (
-                <div key={staff.id} className={this.state.gridDefault}>
-                    <Card>
-                        <CardBody>
-                            <CardTitle>{staff.name}</CardTitle>
-                        </CardBody>
-                    </Card>
-                </div>
-            );
-        });
-        return (
-            <div className='container'>
-                <div className='row'>
-                {staffsList}
-                </div>
-                <p>Bấm vào tên nhân viên để xem thông tin.</p>
-            </div>
-        );
+        <Card>
+          <CardImg width="100%" src={staff.image} alt={staff.name} />
+          <CardBody>
+            <CardTitle>Họ và tên: {staff.name}</CardTitle>
+            <CardText>
+              Ngày sinh: {dateFormat(staff.doB, "dd/mm/yyyy")}
+            </CardText>
+            <CardText>
+              Ngày vào công ty: {dateFormat(staff.startDate, "dd/mm/yyyy")}
+            </CardText>
+            <CardText>Phòng ban: {staff.department.name}</CardText>
+            <CardText>Số ngày nghỉ còn lại: {staff.annualLeave}</CardText>
+            <CardText>Số ngày đã làm thêm: {staff.overTime}</CardText>
+          </CardBody>
+        </Card>
+      );
+    } else {
+      <div>
+        <p>Bấm vào tên nhân viên để xem thông tin.</p>
+      </div>;
     }
+  }
+
+  render() {
+    const staffsList = this.props.staffs.map((staff) => {
+      return (
+        <div key={staff.id} className={this.state.gridDefault}>
+          <Card onClick={() => this.onStaffSelect(staff)}>
+            <CardBody>
+              <CardTitle>{staff.name}</CardTitle>
+            </CardBody>
+          </Card>
+        </div>
+      );
+    });
+    return (
+      <div className="container">
+        <div className="row">{staffsList}</div>
+        <div className="row">{this.onStaffSelect(this.state.selectedStaff)}</div>
+      </div>
+    );
+  }
 }
 
 export default StaffList;
