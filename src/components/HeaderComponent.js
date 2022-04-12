6bfc806cac1e083814e81e2942ac431c1dadf482
Theodor Nguyen <theodornguyen22@gmail.com>
2022-04-12 15:23:42 +0700

    Exercise 2

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -24,9 +24,11 @@ class Header extends Component {
             <div>
                 <Navbar dark expand="md">
                     <div className="container">
-                        <NavbarToggler onClick={this.toggleNav} />
+                     
                         <NavbarBrand className="mr-auto" href="/"><img src='assets/images/logo.png' height="30" width="41" alt='Ristorante Con Fusion' /></NavbarBrand>
-                        <Collapse isOpen={this.state.isNavOpen} navbar>
+                       
+                        <NavbarToggler onClick={this.toggleNav} />
+                         <Collapse isOpen={this.state.isNavOpen} navbar>
                             <Nav navbar>
                             <NavItem>
                                 <NavLink className="nav-link"  to='/home'><span className="fa fa-home fa-lg"></span> Home</NavLink>
