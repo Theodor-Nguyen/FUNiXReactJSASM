5851618fcd95d1a8086aa7e491e4eff1a74ceab0
Theodor Nguyen <theodornguyen22@gmail.com>
2022-04-14 17:56:53 +0700

    Updated sort, searchname function

--- src/components/StaffList.js
+++ src/components/StaffList.js
@@ -14,25 +14,53 @@ const RenderStaffItem = ({ staff }) => {
 };
 
 function StaffList(props) {
-  
-  const [column, setColumn] = useState("col-6 col-md-4 col-lg-2 mt-3 mb-3");
+  const [searchName, setSearchName] = useState("");
+  const [sortId, setSortID] = useState(false);
 
-  const staffList = props.staffs.map((staff) => {
-    return (
-      <div key={staff.id} className={column}>
-        <RenderStaffItem staff={staff} />
-      </div>
-    );
+  const staffList = props.staffs.sort((a,b) => sortId ? a.id - b.id : b.id - a.id)
+  .filter((staff) => {
+      if(searchName === "")
+          return staff;
+      else if(staff.name.toLowerCase().includes(searchName.toLowerCase()))
+          return staff;        
+  }).map((staff) => {
+      return(
+          <div key={staff.id} className="col-6 col-md-4 col-lg-2 mt-3 mb-3">
+              <RenderStaffItem staff={staff} />
+          </div>
+      );
   });
 
+  //Render giao diện Stafflist
   return (
     <div className="container">
-      <div className="row">
-        <div className="col-12 mt-3">
+      <div className="row justify-content-between">
+        <div className="col-4 mt-3">
           <h3>Nhân Viên</h3>
-          <hr />
+        </div>
+        {/* Nút bấm chức năng sắp xếp theo mã số nhân viên */}
+        <div className="col-4 mt-3">
+          <button
+            className="btn btn-success"
+            onClick={() => setSortID(!sortId)}
+          >
+            Sắp xếp theo MSNV
+          </button>
+        </div>
+        {/* Ô tìm kiếm theo dữ liệu  nhân viên */}
+        <div className="col-4 mt-3">
+          <input
+            className="form-control"
+            value={searchName}
+            onChange={(e) => setSearchName(e.target.value)}
+            placeholder="Tìm kiếm nhân viên ..."
+          />
         </div>
       </div>
+      <div className="col-12">
+        <hr />
+      </div>
+
       <div className="row shadow mb-3 mt-3">
       {staffList}
       </div>
