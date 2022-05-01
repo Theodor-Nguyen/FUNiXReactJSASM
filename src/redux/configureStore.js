cdb3e8744c8dc4bfa894587727fafbd8a3ee1897
Theodor Nguyen <theodornguyen22@gmail.com>
2022-05-01 17:02:19 +0700

    React Animations

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -4,23 +4,23 @@ import { Dishes } from "./dishes";
 import { Comments } from "./comments";
 import { Promotions } from "./promotions";
 import { Leaders } from "./leaders";
-import thunk from 'redux-thunk';
-import logger from 'redux-logger';
+import thunk from "redux-thunk";
+import logger from "redux-logger";
 import { InitialFeedback } from "./forms";
 
 export const ConfigureStore = () => {
-    const store = createStore(
-        combineReducers({
-            dishes: Dishes,
-            comments: Comments,
-            promotions: Promotions,
-            leaders: Leaders,
-            ...createForms({
-                feedback: InitialFeedback
-            })
-        }), 
-        applyMiddleware(thunk, logger)
-    );
+  const store = createStore(
+    combineReducers({
+      dishes: Dishes,
+      comments: Comments,
+      promotions: Promotions,
+      leaders: Leaders,
+      ...createForms({
+        feedback: InitialFeedback,
+      }),
+    }),
+    applyMiddleware(thunk, logger)
+  );
 
-    return store;
-}
+  return store;
+};
