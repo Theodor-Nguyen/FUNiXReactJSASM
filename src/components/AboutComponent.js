3faa9881745636e4cd357a7696c5784012273efa
Theodor Nguyen <theodornguyen22@gmail.com>
2022-05-07 14:43:51 +0700

    Exercise 4: task03

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -10,21 +10,31 @@ import {
 import { Link } from "react-router-dom";
 import { Loading } from "./LoadingComponent";
 import { baseUrl } from "../shared/baseUrl";
+import { FadeTransform, Fade, Stagger } from "react-animation-components";
 
 function RenderLeader({ leaders }) {
   const leadersList = leaders.leaders.map((leader) => {
     return (
       <div key={leader.id} className="col-12 mt-5">
-        <Media tag="li">
-          <Media left middle>
-            <Media object src={baseUrl + leader.image} alt={leader.name} />
+        <Fade in>
+          <Media tag="li">
+            <FadeTransform
+              in
+              transformProps={{
+                exitTransform: "scale(0.5) translateY(-50%)",
+              }}
+            >
+              <Media left middle>
+                <Media object src={baseUrl + leader.image} alt={leader.name} />
+              </Media>
+            </FadeTransform>
+            <Media body className="ml-5">
+              <Media heading>{leader.name}</Media>
+              <p>{leader.designation}</p>
+              <p>{leader.description}</p>
+            </Media>
           </Media>
-          <Media body className="ml-5">
-            <Media heading>{leader.name}</Media>
-            <p>{leader.designation}</p>
-            <p>{leader.description}</p>
-          </Media>
-        </Media>
+        </Fade>
       </div>
     );
   });
@@ -36,7 +46,9 @@ function RenderLeader({ leaders }) {
   } else
     return (
       <div className="col-12">
-        <Media list>{leadersList}</Media>
+        <Media list>
+          <Stagger in>{leadersList}</Stagger>
+        </Media>
       </div>
     );
 }
